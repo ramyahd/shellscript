18f37c3a48ea1e29362d9178f8f31d0a9d4fc112
ramyahd <55663832+ramyahd@users.noreply.github.com>
2019-12-09 15:20:53 +0530

    Update Tomcat.sh

--- Tomcat.sh
+++ Tomcat.sh
@@ -1,14 +1,30 @@
 #Go to the folder in which you want to install tomcat
-sudo -i
-
-#Install & unzip the tar.gz folder
-wget https://www.apache.org/dist/tomcat/tomcat-9/v9.0.29/bin/apache-tomcat-9.0.29.tar.gz.as
+#installing java
+sudo yum install java
+#downloading tomcat
+wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
 #unzipping tar file 
-tar -zvxf apache-tomcat-9.0.29.tar.gz
-#install tomcat
-yum install wget -y
+tar -xvzf apache-tomcat-8.5.49.tar.gz
 #remove tomcat downloaded tar file
-rm -rf apache-tomcat-9.0.29.tar.gz
+rm -rf apache-tomcat-8.5.49.tar.gz
+#changing the port for tomcat
+sed -i 's/port="8080"/port="8880"/' apache-tomcat-8.5.49/conf/server.xml
 
-#move to the apache folder
-cd apache-tomcat-9.0.29
+#creating users in tomcat-users.xml
+
+sed -i 's\</tomcat-users>\<!-- -->\g' apache-tomcat-8.5.49/conf/tomcat-users.xml
+
+echo '<role rolename="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<user username="admin" password="admin" roles="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<role rolename="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<user username="script" password="script" roles="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '</tomcat-users>' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+
+#setting valve in comments
+
+sed -i 's/<Valve /<!-- <Valve /' apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
+
+sed -i 's\:1" />\:1" /> -->\g' apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
+
+#running tomcat
+apache-tomcat-8.5.49/bin/./startup.sh
